b66d597a314ef42d9b222b6325d65f0798edebb9
majuss <biomajuss@gmail.com>
2022-01-30 13:11:58 +0100

    push new version

--- lupupy/constants.py
+++ lupupy/constants.py
@@ -1,6 +1,6 @@
 # Used in setup.py
 # -*- coding: utf-8 -*-
-VERSION = "0.1.1"
+VERSION = "0.1.2"
 PROJECT_PACKAGE_NAME = "lupupy"
 PROJECT_LICENSE = "MIT"
 PROJECT_URL = "http://www.github.com/majuss/lupupy"
