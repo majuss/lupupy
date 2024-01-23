e53661711ec7a86470401b5ff22a5000ac66a949
suaveolent <suaveolent@users.noreply.github.com>
2024-01-23 20:15:02 +0100

    bump version

--- lupupy/constants.py
+++ lupupy/constants.py
@@ -1,6 +1,6 @@
 # Used in setup.py
 # -*- coding: utf-8 -*-
-VERSION = "0.3.0"
+VERSION = "0.3.2"
 PROJECT_PACKAGE_NAME = "lupupy"
 PROJECT_LICENSE = "MIT"
 PROJECT_URL = "http://www.github.com/majuss/lupupy"
